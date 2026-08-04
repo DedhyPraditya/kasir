9b202b6f98358e8709120fe2d7158e453197ba01
DedhyPraditya <nurhasdy.nandar@gmail.com>
2026-08-04 23:02:22 +0800

    update mobile pos

--- mobile-pos/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile-pos/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import blue_thermal_printer
+import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BlueThermalPrinterPlugin.register(with: registry.registrar(forPlugin: "BlueThermalPrinterPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
