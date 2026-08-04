bd1085a49c080f934b4a73b9b3915cdef199d6ad
DedhyPraditya <nurhasdy.nandar@gmail.com>
2026-08-04 21:32:06 +0800

    fix: trust all proxies for Cloudflare HTTPS Livewire upload

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'redirect.kasir' => \App\Http\Middleware\RedirectCashierFromAdminPages::class,
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
