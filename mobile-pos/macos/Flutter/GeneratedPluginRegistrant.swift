a2fdde84858a10a4d3944fbb7b96123d6ce91b0f
DedhyPraditya <nurhasdy.nandar@gmail.com>
2026-08-05 01:21:06 +0800

    updpate

--- mobile-pos/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile-pos/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,15 @@ import FlutterMacOS
 import Foundation
 
 import blue_thermal_printer
+import firebase_core
+import firebase_messaging
 import share_plus
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BlueThermalPrinterPlugin.register(with: registry.registrar(forPlugin: "BlueThermalPrinterPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
