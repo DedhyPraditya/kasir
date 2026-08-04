25781191a112e0c68dda37b86631c587946262f2
DedhyPraditya <nurhasdy.nandar@gmail.com>
2026-08-05 00:29:07 +0800

    feat: integrasi Firebase SDK (google-services.json, firebase_core, firebase_messaging)

--- mobile-pos/lib/main.dart
+++ mobile-pos/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp();
+  } catch (_) {}
   runApp(const MyApp());
 }
 
